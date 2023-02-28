e3aa61bfc6b7abf0424c733b08c1151f6782e479
alaodriss <alaodriss@gmail.com>
2023-02-28 13:33:06 +0100

    add persiste for store in local all information

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import store from "./store/Store";
 import { Provider } from "react-redux";
-
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from "./reportWebVitals";
+
+let persistor = persistStore(store);
+export { persistor };
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '../feature/UserSalice';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
-const store = configureStore({
-  reducer: {
-    users: userReducer,
-  },
-})
+const persistConfig = {
+    key: 'user',
+    storage, 
+};
 
-export default store
+const reducers = combineReducers({ users: userReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({ 
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+          serializableCheck: {
+              ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+      }),
+});
